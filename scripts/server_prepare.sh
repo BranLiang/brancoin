c6813637f63e647340d62d39914c0b2d63913302
Bran Liang <lby89757@hotmail.com>
2018-05-30 21:31:31 +0800

    update the script

--- scripts/server_prepare.sh
+++ scripts/server_prepare.sh
@@ -1,7 +1,7 @@
 echo "Reset"
 rm *.db && rm *.dat
 
-echo "1. Set the NODE_ID=3000"
+echo "1. Set the seed NODE_ID=3000"
 export NODE_ID=3000
 
 echo "2. Create wallet"
@@ -14,7 +14,7 @@ echo "4. Backup the blockchain to blockchain_genesis.db"
 cp blockchain_3000.db blockchain_genesis.db
 brancoin listAddresses
 
-echo "5. Set the NODE_ID=3001"
+echo "5. Set the wallet NODE_ID=3001"
 export NODE_ID=3001
 
 echo "6. Create wallets"
@@ -23,4 +23,12 @@ brancoin createWallet
 
 echo "7. Copy genesis blockchain"
 cp blockchain_genesis.db blockchain_3001.db
+
+echo "8. Set the miner node NODE_ID=3002"
+export NODE_ID=3001
+
+echo "9. Create miner wallet"
+brancoin createWallet
+
+echo "10. Copy genesis blockchain"
 cp blockchain_genesis.db blockchain_3002.db
